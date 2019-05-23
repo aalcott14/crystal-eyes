c0e1fe958c70f715e91a9be1d0d13fb1fd75e1cc
Adam Alcott <aalcott14@gmail.com>
2019-05-22 22:01:02 -0500

    webcam images being predicted

--- src/components/ImageCapture/index.tsx
+++ src/components/ImageCapture/index.tsx
@@ -1,14 +1,65 @@
 import React, { ChangeEvent } from 'react';
+import Webcam from 'react-webcam';
+import { Button } from '@material-ui/core';
+
+const urltoFile = (url: string, filename: string, mimeType: string) => {
+  return (fetch(url)
+    .then(function(res){return res.arrayBuffer();})
+    .then(function(buf){return new File([buf], filename, {type:mimeType});})
+  );
+}
+
 
 interface IProps {
   handleImageUpload: (img: File) => void;
 }
 
-class ImageCapture extends React.Component<IProps, {}> {
+interface IState {
+  screenshot: string;
+}
+
+class ImageCapture extends React.Component<IProps, IState> {
+  webcam: Webcam = null;
+
+  state: IState = {
+    screenshot: null
+  }
+
+  handleClick = async () => {
+    const screenshot = this.webcam.getScreenshot();
+    const file = await urltoFile(screenshot, 'crystal.png', 'image/png')
+    this.setState({ screenshot });
+    this.props.handleImageUpload(file);
+  }
+  
   render() {
     return (
-      <div className="d-flex mt-4 flex-column justify-content-center">
-        <input type="file" onChange={(e: ChangeEvent<HTMLInputElement>) => this.props.handleImageUpload(e.target.files[0])} />
+      <div className="d-flex mt-4 flex-column ml-auto mr-auto">
+        <Webcam
+          className="ml-auto mr-auto"
+          height={350}
+          width={350}
+          audio={false}
+          ref={node => this.webcam = node}
+          screenshotFormat="image/png"
+        />
+        <Button 
+          className="mr-auto ml-auto"
+          style={{width: 350}}
+          variant="contained"
+          color="secondary"
+          onClick={this.handleClick}
+        >
+          Capture
+        </Button>
+        <div className="mt-2 ml-auto mr-auto">
+          or
+        </div>
+        <input 
+          className="mt-2 ml-auto mr-auto text-align-center"
+          type="file" 
+          onChange={(e: ChangeEvent<HTMLInputElement>) => this.props.handleImageUpload(e.target.files[0])}
+        />
       </div>
     )
   }
